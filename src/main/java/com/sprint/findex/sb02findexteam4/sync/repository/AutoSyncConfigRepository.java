bbc5b5c726f277056436252ac278c39f0b8606cc
JongwonLee <gudtksfl@gmail.com>
2025-04-22 18:51:02 +0900

    [Feat] BasicAutoSyncConfigService update 메서드 구현

---
+++ src/main/java/com/sprint/findex/sb02findexteam4/sync/repository/AutoSyncConfigRepository.java
@@ -0,0 +1,8 @@
+package com.sprint.findex.sb02findexteam4.sync.repository;
+
+import com.sprint.findex.sb02findexteam4.sync.entity.AutoSyncConfig;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AutoSyncConfigRepository extends JpaRepository<AutoSyncConfig, Long> {
+
+}
