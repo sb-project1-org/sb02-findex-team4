568c8fdeb7c09969189c98a14ead2043a01bb3a1
JongwonLee <gudtksfl@gmail.com>
2025-04-23 15:47:15 +0900

    [Feat] BasicAutoSyncConfigService update Controller 구현

---
+++ src/main/java/com/sprint/findex/sb02findexteam4/sync/dto/AutoSyncConfigUpdateCommand.java
@@ -0,0 +1,8 @@
+package com.sprint.findex.sb02findexteam4.sync.dto;
+
+public record AutoSyncConfigUpdateCommand(
+    Long id,
+    boolean enabled
+) {
+
+}
--- src/main/java/com/sprint/findex/sb02findexteam4/sync/service/BasicAutoSyncConfigService.java
+++ src/main/java/com/sprint/findex/sb02findexteam4/sync/service/BasicAutoSyncConfigService.java
@@ -5,9 +5,7 @@ import com.sprint.findex.sb02findexteam4.exception.InvalidRequestException;
 import com.sprint.findex.sb02findexteam4.exception.NotFoundException;
 import com.sprint.findex.sb02findexteam4.exception.SystemException;
 import com.sprint.findex.sb02findexteam4.sync.dto.AutoSyncConfigDto;
-import com.sprint.findex.sb02findexteam4.sync.dto.AutoSyncConfigFindCommand;
 import com.sprint.findex.sb02findexteam4.sync.dto.AutoSyncConfigUpdateCommand;
-import com.sprint.findex.sb02findexteam4.sync.dto.CursorPageResponseAutoSyncConfigDto;
 import com.sprint.findex.sb02findexteam4.sync.entity.AutoSyncConfig;
 import com.sprint.findex.sb02findexteam4.sync.repository.AutoSyncConfigRepository;
 import lombok.RequiredArgsConstructor;
@@ -22,22 +20,26 @@ public class BasicAutoSyncConfigService implements AutoSyncConfigService {
 
   @Override
   @Transactional
-  public AutoSyncConfigDto update(AutoSyncConfigUpdateCommand command) {
+  public AutoSyncConfigDto update(
+      AutoSyncConfigUpdateCommand command) {
     try {
+      //autoSyncConfig 있는지 확인, 없으면 not found 에러 발생
       AutoSyncConfig autoSyncConfig = autoSyncConfigRepository.findById(command.id()).orElseThrow(
           () -> new NotFoundException(ErrorCode.AUTO_SYNC_NOT_FOUND)
       );
+      //enabled 상태를 업데이트함
       autoSyncConfig.update(command.enabled());
+
+      //build 패턴을 이용해서 dto 감싸기
       return AutoSyncConfigDto.of(autoSyncConfig);
+
+      //예상치 못한 인수가 들어오면 bad request 에러 발생
     } catch (IllegalArgumentException e) {
       throw new InvalidRequestException(ErrorCode.AUTO_SYNC_BAD_REQUEST);
+
+      //시스템이 중간에 뻗는 등이 일이 발생하면 internal server 에러 발생
     } catch (RuntimeException e) {
       throw new SystemException(ErrorCode.INTERNAL_SERVER_ERROR);
     }
-  }
-
-  @Override
-  public CursorPageResponseAutoSyncConfigDto findAll(AutoSyncConfigFindCommand command) {
-    return null;
   }
 }
