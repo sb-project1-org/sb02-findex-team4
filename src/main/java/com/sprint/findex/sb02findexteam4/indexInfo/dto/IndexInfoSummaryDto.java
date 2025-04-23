6edb873ad0a46066dc18f774190612c59f746f79
ddday366 <ddday366@gmail.com>
2025-04-23 17:35:17 +0900

    [Fix] IndexInfoSummaryDto 필드명 수정

--- src/main/java/com/sprint/findex/sb02findexteam4/indexInfo/dto/IndexInfoSummaryDto.java
+++ src/main/java/com/sprint/findex/sb02findexteam4/indexInfo/dto/IndexInfoSummaryDto.java
@@ -2,7 +2,7 @@ package com.sprint.findex.sb02findexteam4.indexInfo.dto;
 
 public record IndexInfoSummaryDto(
     Long id,
-    String IndexClassification,
+    String IndexClassificationName,
     String IndexName
 ) {
 }
