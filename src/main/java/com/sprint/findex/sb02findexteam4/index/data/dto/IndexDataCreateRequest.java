5c3d7c11f46547c50cd3f58099a5432e54c2d1a2
JongwonLee <gudtksfl@gmail.com>
2025-04-25 13:52:22 +0900

    [Feat] 스케쥴러 -> SyncJobApiUseCase 1차 구현 완료

--- src/main/java/com/sprint/findex/sb02findexteam4/index/data/dto/IndexDataCreateRequest.java
+++ src/main/java/com/sprint/findex/sb02findexteam4/index/data/dto/IndexDataCreateRequest.java
@@ -1,7 +1,11 @@
 package com.sprint.findex.sb02findexteam4.index.data.dto;
 
+import com.sprint.findex.sb02findexteam4.sync.dto.IndexDataFromApi;
+import com.sprint.findex.sb02findexteam4.util.TimeUtils;
 import java.math.BigDecimal;
+import lombok.Builder;
 
+@Builder
 public record IndexDataCreateRequest(
     Long indexInfoId,
     String baseDate,
@@ -16,4 +20,18 @@ public record IndexDataCreateRequest(
     Long marketTotalAmount
 ) {
 
+  public static IndexDataCreateRequest from(Long indexInfoId, IndexDataFromApi dataFromApi) {
+    return IndexDataCreateRequest.builder()
+        .indexInfoId(indexInfoId)
+        .baseDate(TimeUtils.normalizeToDashedDate(dataFromApi.baseDate()))
+        .marketPrice(dataFromApi.marketPrice())
+        .closingPrice(dataFromApi.closingPrice())
+        .highPrice(dataFromApi.highPrice())
+        .lowPrice(dataFromApi.lowPrice())
+        .versus(dataFromApi.versus())
+        .fluctuationRate(dataFromApi.fluctuationRate())
+        .tradingQuantity(dataFromApi.tradingQuantity())
+        .tradingPrice(dataFromApi.tradingPrice())
+        .marketTotalAmount(dataFromApi.marketTotalAmount()).build();
+  }
 }
--- src/main/java/com/sprint/findex/sb02findexteam4/sync/service/SyncJobApiUseCase.java
+++ src/main/java/com/sprint/findex/sb02findexteam4/sync/service/SyncJobApiUseCase.java
@@ -1,5 +1,14 @@
 package com.sprint.findex.sb02findexteam4.sync.service;
 
+import com.sprint.findex.sb02findexteam4.sync.dto.SyncJobHistoryDto;
+import java.util.List;
+
 public interface SyncJobApiUseCase {
+
+  void syncAll();
+
+  List<SyncJobHistoryDto> syncIndexInfoFromApi();
+
+  List<SyncJobHistoryDto> syncIndexDataFromApi(String baseDateFrom, String baseDateTo);
 
 }
