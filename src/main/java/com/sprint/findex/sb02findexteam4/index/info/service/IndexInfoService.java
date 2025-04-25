663f6fc45a632d36f955459a9c1d4873a59f9ce7
ddday366 <ddday366@gmail.com>
2025-04-25 15:20:53 +0900

    [Fix] IndexInfoService 수정

--- src/main/java/com/sprint/findex/sb02findexteam4/index/info/service/IndexInfoService.java
+++ src/main/java/com/sprint/findex/sb02findexteam4/index/info/service/IndexInfoService.java
@@ -3,18 +3,20 @@ package com.sprint.findex.sb02findexteam4.index.info.service;
 import com.sprint.findex.sb02findexteam4.index.info.dto.IndexInfoCreateCommand;
 import com.sprint.findex.sb02findexteam4.index.info.dto.IndexInfoCreateRequest;
 import com.sprint.findex.sb02findexteam4.index.info.dto.IndexInfoDto;
+import com.sprint.findex.sb02findexteam4.index.info.dto.IndexInfoSummaryDto;
 import com.sprint.findex.sb02findexteam4.index.info.dto.IndexInfoUpdateRequest;
 import com.sprint.findex.sb02findexteam4.index.info.entity.IndexInfo;
 import com.sprint.findex.sb02findexteam4.index.info.entity.SourceType;
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface IndexInfoService {
 
   IndexInfoDto registerIndexInfo(IndexInfoCreateRequest requestDto, SourceType sourceType);
-  IndexInfo registerIndexInfoFromApi(IndexInfoCreateCommand requestDto);
-  Page<IndexInfoDto> getIndexInfoWithFilters(String classificationName, String indexName,
-      Boolean favorite, Pageable pageable);
+  IndexInfo registerIndexInfoFromApi(IndexInfoCreateCommand command);
+//  Page<IndexInfoSummaryDto> getIndexInfoWithFilters();
+  List<IndexInfoSummaryDto> getIndexInfoSummaries();
   IndexInfoDto findById(Long id);
   IndexInfoDto updateIndexInfo(Long id, IndexInfoUpdateRequest updateDto);
   void deleteIndexInfo(Long id);
