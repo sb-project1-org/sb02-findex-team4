d24f9ce89eaaf150791cdf19dfd928efb4a0092b
JongwonLee <gudtksfl@gmail.com>
2025-04-26 11:34:10 +0900

    [Fix] 잘못 추가된 내용 수정

--- src/main/java/com/sprint/findex/sb02findexteam4/index/info/repository/IndexInfoRepository.java
+++ src/main/java/com/sprint/findex/sb02findexteam4/index/info/repository/IndexInfoRepository.java
@@ -9,8 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IndexInfoRepository extends JpaRepository<IndexInfo, Long>,
-    CustomIndexInfoRepository {
+public interface IndexInfoRepository extends JpaRepository<IndexInfo, Long> {
 
   List<IndexInfo> findAllByFavoriteTrue();
 
